2c8bcb55650d0d7872f410a676b0fa2ec218080e
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-07-14 23:43:35 +0530

    To do list project

---
+++ Day 12/main.js
@@ -0,0 +1,20 @@
+let addToDoButton= document.getElementById("add");
+let toDoContainer= document.getElementById("addcontainer");
+let inp = document.getElementById("in");
+let clear=document.getElementById("delete");
+console.log(inp);
+addToDoButton.addEventListener('click',myFun);
+inp.addEventListener('keyup',myFun());
+function myFun(){
+    var paragraph= document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);
+    inp.value = " ";
+    paragraph.addEventListener("click",function(){
+        paragraph.style.textDecoration="line-through";
+    });
+    clear.addEventListener("click",function(){
+        paragraph.remove();
+    });
+    
+};
