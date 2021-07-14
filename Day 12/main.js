050a4431727a6ca90bf5f25ace54ccd8f06dde4c
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-07-15 00:21:18 +0530

    bg changes

--- Day 12/main.js
+++ Day 12/main.js
@@ -2,11 +2,11 @@ let addToDoButton= document.getElementById("add");
 let toDoContainer= document.getElementById("addcontainer");
 let inp = document.getElementById("in");
 let clear=document.getElementById("delete");
-console.log(inp);
 addToDoButton.addEventListener('click',myFun);
 inp.addEventListener('keyup',myFun());
 function myFun(){
     var paragraph= document.createElement('p');
+    paragraph.innerText= inp.value;
     paragraph.classList.add('paragraph-styling');
     toDoContainer.appendChild(paragraph);
     inp.value = " ";
