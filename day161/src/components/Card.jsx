e4db9d368cc789f9438bb52d65aa0964850ae9d7
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-07-24 23:19:10 +0530

    chess bord

---
+++ day161/src/components/Card.jsx
@@ -0,0 +1,10 @@
+const Card =() =>{
+    return(
+        <div className="card">
+            <img src="https://img.devrant.com/devrant/rant/r_858966_EiSt4.jpg" alt="no imag" />
+            <h2>React card </h2>
+            <p>This is a card</p>
+        </div>
+    );
+}
+export default Card;
